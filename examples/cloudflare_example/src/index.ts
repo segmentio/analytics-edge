125c7d632aca052f4d528f1346bd6e1162ba7948
Pooya Jaferian <pooya.j@gmail.com>
2022-09-15 21:34:37 -0700

    cleanup

--- examples/cloudflare_example/src/index.ts
+++ examples/cloudflare_example/src/index.ts
@@ -7,22 +7,21 @@ declare global {
   const PERSONAS_TOKEN: string;
 }
 
-const writeKey = SEGMENT_WRITE_KEY;
-const personasSpaceId = PERSONAS_SPACE_ID;
-const personasToken = PERSONAS_TOKEN;
-
-const segment = new Segment(
-  writeKey,
-  "seg",
-  true,
-  personasSpaceId,
-  personasToken
-);
-// export default segment.init();
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    const writeKey = env.SEGMENT_WRITE_KEY;
+    const personasSpaceId = env.PERSONAS_SPACE_ID;
+    const personasToken = env.PERSONAS_TOKEN;
+
+    const segment = new Segment(
+      writeKey,
+      "seg",
+      true,
+      personasSpaceId,
+      personasToken
+    );
+
     return segment.handle(request, env);
   },
 };
