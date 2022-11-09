8272b4eeb182adc32a169263bb13d59b61d9a4df
Pooya Jaferian <pooya.j@gmail.com>
2022-11-09 12:04:15 -0800

    remove edge functions for now

--- packages/edge-sdk-cloudflare/src/tapi.ts
+++ packages/edge-sdk-cloudflare/src/tapi.ts
@@ -1,42 +1,5 @@
 import { Env, HandlerFunction } from "./types";
 
-export const handleEdgeFunctions: HandlerFunction = async (
-  request,
-  response,
-  context
-) => {
-  let body: { [key: string]: any } = await request.json();
-  const writeKey = context.settings.writeKey;
-  // getting edge functions
-  const edgeFunctions = await context.env.EdgeFunctions.list({
-    prefix: writeKey,
-  });
-
-  const edgePointers = edgeFunctions.keys
-    .map((key: { name: string }) => key.name)
-    .map((key: string) => key.replace(`${writeKey}-`, ""));
-
-  for (const func of edgePointers) {
-    let user_worker = context.env.dispatcher.get(func);
-    console.log("invoking", func);
-    const data = await user_worker.fetch(
-      new Request(request.url, { body: JSON.stringify(body), method: "POST" })
-    );
-    body = await data.json();
-  }
-
-  return [
-    new Request(request.url, {
-      body: JSON.stringify(body),
-      headers: request.headers,
-      cf: request.cf as RequestInitCfProperties,
-      method: request.method,
-    }),
-    response,
-    context,
-  ];
-};
-
 export const enrichEdgeTraits: HandlerFunction = async (
   request,
   response,
