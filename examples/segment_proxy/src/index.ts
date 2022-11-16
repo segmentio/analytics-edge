8445e504bfdd33a56eeba37be25a0f3f69f24229
Pooya Jaferian <pooya.j@gmail.com>
2022-11-16 11:10:42 -0800

    adding proxy example

---
+++ examples/segment_proxy/src/index.ts
@@ -0,0 +1,38 @@
+import { Segment } from "@segment/edge-sdk-cloudflare";
+import type { Env as SDKEnv } from "@segment/edge-sdk-cloudflare";
+
+export interface Env extends SDKEnv {
+  SEGMENT_WRITE_KEY: string;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const segment = new Segment(
+      {
+        writeKey: env.SEGMENT_WRITE_KEY,
+        routePrefix: "magic",
+        collectEdgeData: true,
+      },
+      env,
+      {
+        useProfilesAPI: false,
+        ajsInjection: false,
+        edgeVariations: false,
+        clientSideTraits: false,
+        proxyOrigin: false,
+      }
+    );
+
+    const resp = await segment.handleEvent(request, env);
+
+    if (resp) {
+      return resp;
+    } else {
+      return new Response("no response");
+    }
+  },
+};
