929d22b1a82786fd10e47f8876fcae0ea72f2ca7
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-09-06 13:56:17 -0500

    remove unneeded eslint

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `@segment/eslint-config-edge-sdk`
-  extends: ["@segment/edge-sdk"],
-};
--- packages/edge-sdk/.eslintrc.js
+++ packages/edge-sdk/.eslintrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-  root: true,
   extends: ["@segment/edge-sdk"],
 };
